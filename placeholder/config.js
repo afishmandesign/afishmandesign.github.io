bd06f4cc45b13d532897dff1221679f5f2b4e15f
DESKTOP-6AF44U4\fordf <fordfishman@gmail.com>
2021-01-12 18:23:57 -0500

    New file structure for animations

---
+++ placeholder/config.js
@@ -0,0 +1,21 @@
+define({
+  "SV": {
+    "svData":[
+      {
+          "svKey": "message_text", 
+          "svType": "string",
+          "value": "VARIABLE TEXT LINE 1"
+      },
+      {
+          "svKey": "message2_text", 
+          "svType": "string",
+          "value": "Variable Text Line 2"
+      },
+      { 
+          "svKey": "cta_btn_text", 
+          "svType": "string",
+          "value": "VAR BTN TEXT"
+      }
+    ]
+  }
+});
